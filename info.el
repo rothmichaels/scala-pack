d15c379a204670a25f92a9b2989eb5bdff3033a7
Roth Michaels <roth@rothmichaels.us>
2015-11-06 13:30:56 -0500

    0.1.0

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
 (live-pack-name "scala-pack")
-(live-pack-version "0.0.1")
+(live-pack-version "0.1.0")
 (live-pack-description "Scala and SBT support for emacs-live.")
--- init.el
+++ init.el
@@ -3,5 +3,11 @@
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
 
+(live-add-pack-lib "scala-mode2")
+(require 'scala-mode2)
+
+(live-add-pack-lib "sbt-mode")
+(require 'sbt-mode)
+
 ;; Load bindings config
 (live-load-config-file "bindings.el")
